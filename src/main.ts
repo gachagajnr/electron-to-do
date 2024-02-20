1590cce8b020efd8039c1e957bb91dc2cffbde69
gachagajnr <williampius17@users.noreply.github.com>
2024-02-20 12:45:41 +0300

    initial

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+const { app, BrowserWindow } = require("electron");
+ import * as path from "path";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname + "/index.html"));
+
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
